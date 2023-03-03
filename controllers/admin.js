6ad61ccb9f72fc5ed1ee1edbb5ba3195dc30c2fc
ishitaanand2222 <ishitaanand2222@gmail.com>
2023-03-03 22:45:24 +0530

    Update and Delete products using sequelize | Mod 11

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-
 exports.getAddProduct = (req, res, next) => {
     res.render('admin/edit-product',{
         pageTitle: 'Add Product', 
@@ -20,6 +19,7 @@ exports.postAddProduct = (req,res,next) => {
         description: description
     }).then( result => {
         console.log('Product created');
+        res.redirect('/admin/products');
     }).catch( err => {
         console.log(err);
     })
@@ -31,17 +31,20 @@ exports.getEditProduct = (req, res, next) => {
         return res.redirect('/');
     }
     const prodId = req.params.productId;
-    Product.findById(prodId, product => {
-        if(!product){
-            return res.redirect('/');
-        }
+    console.log(prodId);
+    Product.findByPk(prodId)
+    .then(product => {
+        if(!product)
+        return res.redirect('/');
+
         res.render('admin/edit-product',{
-        pageTitle: 'Edit Product', 
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product
-    });
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        })
     })
+    .catch(err => {console.log(err)});
 }
 
 //here we will construct a new product and will replace the existing one with the new product
@@ -51,10 +54,20 @@ exports.postEditProduct = (req,res,next) => {
     const updatedPrice = req.body.price;
     const updatedImageUrl = req.body.imageUrl;
     const updatedDesc = req.body.description;
-    const updatedProduct = new Product(prodId,updatedTitle,updatedImageUrl,updatedDesc,updatedPrice);
-
-    updatedProduct.save();
-    res.redirect('/admin/products');
+    Product.findByPk(prodId)
+    .then(product => {
+        product.title= updatedTitle;
+        product.price = updatedPrice;
+        product.description = updatedDesc;
+        product.imageUrl = updatedImageUrl;
+        return product.save();//this save() method provided by sequalize will either update the existing product or will create a new product, if the product doesnt exist
+        //save() will return a promise and here we just return that promise.
+    })
+    .then(result => {//second promise from save
+        console.log('UPDATED PRODUCT!');
+        res.redirect('/admin/products');    
+    })
+    .catch(err => {console.log(err)});//this will catch errors from both the then blocks
 }
 
 exports.getProducts = (req,res,next) => {
@@ -63,7 +76,7 @@ exports.getProducts = (req,res,next) => {
         res.render('admin/products',{
             prods: products,
             pageTitle: 'Admin Products',
-            path: 'admin/products'
+            path: '/admin/products'
         })
     })
     .catch(err => console.log(err));
@@ -71,6 +84,14 @@ exports.getProducts = (req,res,next) => {
 
 exports.postDeleteProduct = (req,res,next) => {
     const prodId = req.body.productId;
-    Product.deleteById(prodId);
-    res.redirect('/admin/products');
+    //Product.destroy({}), this is one way of destroying products, in the curly braces we can the pass the where condition to filter which product to delete
+    Product.findByPk(prodId)
+    .then(product => {
+        return product.destroy();
+    }) 
+    .then(result => {//this is the then block for destroy() method
+        console.log('PRODUCT DELETED');
+        res.redirect('/admin/products');
+    })
+    .catch(err =>  console.log(err));
 }
