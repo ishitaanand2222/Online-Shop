b9c50d468b6322e04b37fc24da0c7740cf4e16d0
ishitaanand2222 <ishitaanand2222@gmail.com>
2023-04-12 18:59:09 +0530

    Editing our product

--- controllers/admin.js
+++ controllers/admin.js
@@ -33,29 +33,27 @@ exports.postAddProduct = (req,res,next) => {
     })
 }
 
-// exports.getEditProduct = (req, res, next) => {
-//     const editMode = req.query.edit;//here we are accessing the query. The extracted value is always a string 
-//     if(!editMode){//if editMode is false
-//         return res.redirect('/');
-//     }
-//     const prodId = req.params.productId;
-//     req.user
-//     .getProducts({where: {id: prodId}})
-//     //Product.findByPk(prodId)
-//     .then(products => {
-//         const product = products[0];
-//         if(!product)
-//         return res.redirect('/');
+exports.getEditProduct = (req, res, next) => {
+    const editMode = req.query.edit;//here we are accessing the query. The extracted value is always a string 
+    if(!editMode){//if editMode is false
+        return res.redirect('/');
+    }
+    const prodId = req.params.productId;
+    
+    Product.findByPk(prodId)
+    .then(product => {
+        if(!product)
+        return res.redirect('/');
 
-//         res.render('admin/edit-product',{
-//             pageTitle: 'Edit Product',
-//             path: '/admin/edit-product',
-//             editing: editMode,
-//             product: product
-//         })
-//     })
-//     .catch(err => {console.log(err)});
-// }
+        res.render('admin/edit-product',{
+            pageTitle: 'Edit Product',
+            path: '/admin/edit-product',
+            editing: editMode,
+            product: product
+        })
+    })
+    .catch(err => {console.log(err)});
+}
 
 
 //here we will construct a new product and will replace the existing one with the new product
@@ -82,8 +80,7 @@ exports.postEditProduct = (req,res,next) => {
 }
 
 exports.getProducts = (req,res,next) => {
-    req.user
-    .getProducts()
+    Product.fetchAll()
     .then(products => {
         res.render('admin/products',{
             prods: products,
@@ -91,7 +88,7 @@ exports.getProducts = (req,res,next) => {
             path: '/admin/products'
         })
     })
-    .catch(err => console.log(err));
+    .catch(err => console.log(err))
 }
 
 exports.postDeleteProduct = (req,res,next) => {
